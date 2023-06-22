bf7490ca2600813b2e061cf081eb09a4d0d7d040
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-22 15:35:52 +0500

    add Pizza Block component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import { Header } from './components/index.js';
 import { Home, Cart } from './pages/index.js';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3001/db.json')
+      .then((resp) => resp.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </div>
