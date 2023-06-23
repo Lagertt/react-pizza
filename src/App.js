658a11a4534d8c09738e14ee6e693d29f7d5a778
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-23 17:31:37 +0500

    add build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:3001/db.json')
+    fetch('http://localhost:3000/db.json')
       .then((resp) => resp.json())
       .then((json) => setPizzas(json.pizzas));
   }, []);
